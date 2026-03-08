1f7da12c69e50d5322779831dd722b1b051aad00
3Dever96 <134976722+3Dever96@users.noreply.github.com>
2026-03-08 16:29:48 -0400

    Solo Network established

---
+++ Assets/Scripts/System/GameManager.cs
@@ -0,0 +1,41 @@
+using Unity.Netcode;
+using Unity.Netcode.Transports;
+using Unity.Netcode.Transports.SinglePlayer;
+using UnityEngine;
+
+namespace VentureBound.System
+{
+    public class GameManager : MonoBehaviour
+    {
+        public static GameManager instance;
+
+        public void Start()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+            else
+            {
+                if (instance != this)
+                {
+                    Destroy(gameObject);
+                }
+            }
+
+            DontDestroyOnLoad(gameObject);
+
+            StartSoloSession();
+        }
+
+        public void StartSoloSession()
+        {
+            NetworkManager netObject = NetworkManager.Singleton.GetComponent<NetworkManager>();
+            SinglePlayerTransport solo = netObject.GetComponent<SinglePlayerTransport>();
+
+            netObject.NetworkConfig.NetworkTransport = solo;
+
+            netObject.StartHost();
+        }
+    }
+}
