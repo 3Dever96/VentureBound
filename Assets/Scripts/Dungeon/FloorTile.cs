0179c441a4ef8f90d571bed3c08ee2d134220b07
3Dever96 <134976722+3Dever96@users.noreply.github.com>
2026-05-22 14:43:36 -0400

    Made a randomly generated dungeon with platforms in each room

---
+++ Assets/Scripts/Dungeon/FloorTile.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+namespace VentureBound
+{
+    public class FloorTile : MonoBehaviour
+    {
+        public bool isLocked;
+        public Vector2Int gridPos;
+        public RoomObject myRoom;
+
+        public void SetScale(int min, int max)
+        {
+            Vector2Int[] directions = { Vector2Int.up, Vector2Int.down, Vector2Int.right, Vector2Int.left };
+
+            int scale = Random.Range(min, max);
+
+            if (scale != 0)
+            {
+                transform.localScale = new Vector3(1, scale * 2f, 1);
+            }
+
+            if (scale == 3)
+            {
+                int index = Random.Range(0, 4);
+
+                if (myRoom.floorTiles.ContainsKey(gridPos + directions[index]))
+                {
+                    FloorTile neighbor = myRoom.floorTiles[gridPos + directions[index]];
+
+                    if (!neighbor.isLocked && neighbor.transform.localScale.y < 1f)
+                    {
+                        neighbor.SetScale(1, max - 1);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Dungeon/RoomObject.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VentureBound
+{
+    public class RoomObject : MonoBehaviour
+    {
+        public Dictionary<Vector2Int, FloorTile> floorTiles = new Dictionary<Vector2Int, FloorTile>();
+        public List<FloorTile> available = new List<FloorTile>();
+
+        private void Start()
+        {
+            FloorTile[] tiles = GetComponentsInChildren<FloorTile>();
+
+            foreach (FloorTile tile in tiles)
+            {
+                Vector2Int position = new Vector2Int(Mathf.RoundToInt((tile.transform.position.x + 8) / 4), Mathf.RoundToInt((tile.transform.position.z + 8) / 4));
+                tile.gridPos = position;
+                tile.myRoom = this;
+                floorTiles.Add(position, tile);
+
+                if (!tile.isLocked)
+                {
+                    available.Add(tile);
+                }
+            }
+
+            int platforms = 0;
+
+            while (platforms < 3)
+            {
+                int index = Random.Range(0, available.Count);
+
+                FloorTile tile = available[index];
+
+                tile.SetScale(0, 4);
+
+                available.RemoveAt(index);
+
+                platforms++;
+            }
+        }
+    }
+}
